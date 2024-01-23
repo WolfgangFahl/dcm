7b30e98a113ad76693f193f4b1777b67b004a2b4
Wolfgang Fahl <wf@bitplan.com>
2024-01-23 14:42:08 +0100

    prepares release

--- dcm/version.py
+++ dcm/version.py
@@ -17,7 +17,7 @@ class Version:
     name = "dcm"
     version = dcm.__version__
     date = "2023-11-06"
-    updated = "2024-01-21"
+    updated = "2024-01-23"
     description = "python based visualization of dynamic competence maps"
 
     authors = "Wolfgang Fahl"
